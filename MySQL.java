cd9d31d1590399cec4af9da152b15b0f451f0fd8
Shizu <135083266+xT0Blx@users.noreply.github.com>
2023-06-13 00:06:17 +0800

    Add files via upload

--- MySQL.java
+++ MySQL.java
@@ -1,7 +1,16 @@
 package test;
 
 import com.mysql.jdbc.CommunicationsException;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
@@ -37,25 +46,23 @@ public class MySQL {
         createSQL();
     }
 
-    public void getValue(String JUser, int studentID){  
+    public void Login(String JUser, int studentID){  
         boolean flag = true;
-        //The following is an algorithm to search the SQL database for the input values
+        //The following is an algorithm to search the SQL database for the passed values and then stores it if there is a match
         try{
-                ResultSet rs = stmt.executeQuery("select * from StudInfo");  
+                ResultSet rs = stmt.executeQuery("select * from StudInfo"); //This code allows for values to be called from the table
                 while(rs.next()){
-                    String User = rs.getString(1);
-                    int ID = rs.getInt("StudID");
-                    //code to scan database for information(Specifically StudID)
-
+                    String User = rs.getString(1); //Passes the value of the first column of the SQL(the name) into a local variable
+                    int ID = rs.getInt("StudID"); //Passes the value of the specified row of the SQL(the StudID column) into a local variable
+                    
+                    /*Using the created local variables the algorithm below uses it as a condition in the parameter to see 
+                    if the User inputs match any data in the SQL*/
                     if(JUser.equals(User) && studentID==ID){
-			/*The If else statements has a condition in the parameter to check if the username and ID matches the data in the passed on 
-			value(Data obtained from the database specifically Name and studID). If there is a match then it will create a String data 
-			type with the data of the user from the database(The values are obtained using the Result set object 'rs' to call the string 
-			data of the stored information in the database, where the numbers represent the column the data is stored in within the database)
-			This string is then passed on to other methods or objects*/
-                        String studentData = rs.getString(2) + ", " + rs.getString(1) + ", " + rs.getString(4) + ", " + rs.getString(5) ; 
-                        store(studentData);
-                        //method call to store the obtained data
+                        //if there is a match then the ResultSet object 'rs' will call upon the data specified by the type and column indicated
+                        String studentData = rs.getString(2) + ", " + rs.getString(1) + ", " + rs.getString(4) + ", " + rs.getString(5) ;
+                        String category = rs.getString(3);
+                        store(studentData, category); //method call to store the obtained data
+                        
                         JOptionPane.showMessageDialog(null,"Login Succesful! Attended today.");
                         //if matching data is found then it is printed and prompt to announced successful login and attandance status
                         flag = false;
@@ -72,7 +79,7 @@ public class MySQL {
         }  
     } 
     
-    public void setValue(String JName, int JStudID,String JType, String JCourse, String JSection, int JYear){
+    public void Register(String JName, int JStudID,String JType, String JCourse, String JSection, int JYear){
         try{
             String Name = JName;
             int StudID = JStudID;
@@ -94,15 +101,11 @@ public class MySQL {
         }  
     }  
     
-    public void store(String str){
+    public void store(String studInfo, String studType){
         String timeNow = new SimpleDateFormat("KK:mma").format(new java.util.Date()); //Time format where KK=hour(01) mm=minutes and a is am/pm
-
-        String sqlData = str;
-        Attendance entry = new Attendance();
         
-        entry.attendanceTracker(sqlData + " /" + timeNow);
+        LoginTracker(studInfo + " /" + timeNow, studType);
         //A method to store the obtained data from the register log in to the Attendance java class (Includes log in time)
-        
     }
     public void createSQL(){
         try {
@@ -152,16 +155,25 @@ public class MySQL {
         }
     }
     
-    public void createFolders(){
+    public void createFolders(){ //Sets up neccessary Folders Directories needed for the back end codes
         File folder = new File("C:\\Attendance"); 
-        File newAttendance = new File("C:\\Attendance\\AttendanceRecords");
+        File tertiatyAttendance = new File("C:\\Attendance\\Tertiary_Records");
+        File shsAttendance = new File("C:\\Attendance\\SHS_Records");
         //Path file objects to direct the java program to create the files in the specified paths
         
         //The parameter/conditon checks if the folder exist in the path given, if not then created the folder
         if(folder.mkdir() == true) {
             
-            if(newAttendance.mkdir() == true) { 
-                System.out.println("Directories has been created successfully!"); 
+            if(tertiatyAttendance.mkdir() == true) { 
+                System.out.println("Tertiary folder has been created successfully!"); 
+                //Does the same thing but checks for a different folder in a different path
+            }
+            else{
+                System.out.println("failed to create the second directory");
+                //Informs if there was a failed attempt
+            }
+            if(shsAttendance.mkdir() == true) { 
+                System.out.println("SHS folder has been created successfully!"); 
                 //Does the same thing but checks for a different folder in a different path
             }
             else{
@@ -178,4 +190,47 @@ public class MySQL {
         /*You can call this blank method to check the System if the needed database is present
         If not then an error is thrown via the constructor of the class*/
     }
+    
+    public void LoginTracker(String attendanceData, String type){
+        /*This method is called in another method or Jframe that gives it a string data which endcodes the given 
+        data(expected to be the student information from the MySQL java class file) in a txt file.
+        The date is set to the current date from the computer and adds it to the name of the txt file for easier identification.
+
+        The method list all the acquired data into a txt file that will be continuesly added provided that it is given data and matches
+        the date of the txt file created, otherwise creates a new one and adds to that one instead*/
+        
+        try{
+            if(type.equals("Tertiary")){
+                String d = new SimpleDateFormat("MMMddyyyy").format(new java.util.Date()); 
+                //Date format where MMM=month(Jun) dd=day and yyyy=year
+                Path TfilePath = Paths.get("C:\\Attendance\\Tertiary_Records\\StudentLogins" + d + ".txt");
+                //sets up the filepath and file name using Path object
+
+                OutputStream output = null;
+                output = new BufferedOutputStream(Files.newOutputStream(TfilePath,CREATE, APPEND));
+                BufferedWriter input = new BufferedWriter(new OutputStreamWriter(output));
+                input.write(attendanceData);
+                input.newLine();
+                input.close();
+                //Buffered writer object and built in methods to encode the login data into storable txt file
+            }
+            if(type.equals("SHS")){ //If the type of student is SHS then this c
+                String d = new SimpleDateFormat("MMMddyyyy").format(new java.util.Date()); 
+                //Date format where MMM=month(Jun) dd=day and yyyy=year
+                Path SfilePath = Paths.get("C:\\Attendance\\SHS_Records\\StudentLogins" + d + ".txt");
+                //sets up the filepath and file name using Path object
+
+                OutputStream output = null;
+                output = new BufferedOutputStream(Files.newOutputStream(SfilePath,CREATE, APPEND));
+                BufferedWriter input = new BufferedWriter(new OutputStreamWriter(output));
+                input.write(attendanceData);
+                input.newLine();
+                input.close();
+                //Buffered writer object and built in methods to encode the login data into storable txt file
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error found: " + e);
+        }
+    }   
 }
